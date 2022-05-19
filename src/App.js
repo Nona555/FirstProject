ff0629b9d2ae5827a2feaf5c4cd7efc6c3685954
Nona555 <nona.karapetyan2001@gmail.com>
2022-05-19 20:50:31 +0400

    Timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer/Timer';
+import Functional from './Functional/Functional';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Functional name={'Functional Component'} age={5} />
+      <Timer name={'Timer'} />
     </div>
   );
 }
