90a2d051c4803af5c98448e9287d12ec90f4a950
Nona555 <nona.karapetyan2001@gmail.com>
2022-06-09 15:34:51 +0400

    Local

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,29 +1,46 @@
-import { headerData } from '../constants';
 import './Header.scss';
-import classNames from 'classnames';
+import profile from '../assets/icons/user.png'
+import user from '../assets/icons/uss.png'
+import key from '../assets/icons/key1.png'
+import dash from '../assets/icons/dash.png'
 
-function Header({ bgColor, color, icon, buttonBg }) {
+import userEvent from '@testing-library/user-event';
+
+function Header() {
     return (
-        <div className={classNames('header', {
-            'transparentBackground': bgColor
-        })}>
-            <div className='inner_header' style={{ background: bgColor && bgColor }}>
+        <div className='header'>
+            <div className='inner_header'>
                 <div className='logo_container'>
-                    <p>Soft UI Dashbord </p>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                    <p> Soft UI Dashbord </p>
+                    </div>
+                    <ul className='navigation'>
+                     <a><li>
+                     <img style={{width: 18, marginRight: 5}} src={dash}/>
+                        Dashboard
+                        </li></a>
+                     <a><li>
+                        <img style={{width: 18, marginRight: 5}} src={profile}/>
+                        Profile
+                    </li></a>
+                     <a><li>
+                       <img style={{width:18,marginRight:5}} src={user}/>
+                        Sign Up 
+                        </li></a>
+                     <a><li>
+                     <img style={{width:18,marginRight:5}} src={key}/>
+                        
+                        Sign In 
+                        
+                        </li></a>
+                    </ul>
+                    <button className='button'>FREE DOWNLOAD</button>
                 </div>
-                <ul className='navigation'>
-                    {headerData.map(item =>
-                    <a href={item.path}>
-                        <li>
-                            <img src={icon ? item.whiteIcon : item.blackIcon} alt={item.iconAlt} />
-                            <span style={{color: color && color}}>{item.name}</span>
-                        </li>
-                    </a>)}
-                </ul>
-                <button className={classNames('button',
-                {'lightBackground': buttonBg})}>FREE DOWNLOAD</button>
             </div>
-        </div>   
+           
+        
     )
 }
 
